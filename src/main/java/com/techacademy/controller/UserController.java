064b9fc41bf3f50a1e951dfb213cab33dbefa7a8
ayakaoosugi <tantaran924@gmail.com>
2023-02-25 14:56:42 +0900

    課題提出

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.techacademy.controller;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
@@ -37,14 +42,21 @@ public class UserController {
         return "user/register";
     }
 
+    // ----- 変更ここから -----
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
+  
+
     // ----- 追加:ここまで -----
 //----- 追加:ここから -----
 /** User更新画面を表示 */
@@ -58,11 +70,27 @@ public String getUser(@PathVariable("id") Integer id, Model model) {
 
 /** User更新処理 */
 @PostMapping("/update/{id}/")
-public String postUser(User user) {
+public String postUser(@Validated User user, BindingResult res, Model model) {
+	if(res.hasErrors()) {
+		  return  "user/update";
+    }
     // User登録
     service.saveUser(user);
     // 一覧画面にリダイレクト
     return "redirect:/user/list";
 }
+
+
+// ----- 追加:ここまで -----
+// ----- 追加:ここから -----
+/** User削除処理 */
+@PostMapping(path="list", params="deleteRun")
+public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+    // Userを一括削除
+    service.deleteUser(idck);
+    // 一覧画面にリダイレクト
+    return "redirect:/user/list";
+}
 // ----- 追加:ここまで -----
 }
+
